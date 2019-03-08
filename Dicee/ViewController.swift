e05d35923a81e4bafac7ea01009e80b79bbf1d19
Admin <admin@Admins-Mac.local>
2019-03-08 11:28:33 +0700

    dice-game

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,8 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dice1: UIImageView!
+    @IBOutlet weak var dice2: UIImageView!
+    
+    var random1 : Int = 0
+    var random2 : Int = 0
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        pollDice()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +28,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func actionRoll(_ sender: Any) {
+        pollDice()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        pollDice()
+    }
+    
+    func pollDice(){
+        random1 = Int(arc4random_uniform(6))
+        random2 = Int(arc4random_uniform(6))
+        dice1.image = UIImage(named: diceArray[random1])
+        dice2.image = UIImage(named: diceArray[random2])
+    }
+    
 }
 
